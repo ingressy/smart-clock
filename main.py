756cce533c0546970d9cce374ee014c4807118af
ingressy <j.czinzoll@ingressy.de>
2024-10-04 19:18:06 +0200

    add weather and update routine

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import digitalio, board, os, openmeteo_requests, requests_cache, requests, time
+from os import getenv
+
+import digitalio, board, os, openmeteo_requests, requests_cache, time, datetime, webuntis.objects
 from lib import epd2in13_V4
 from PIL import Image, ImageDraw, ImageFont
 from retry_requests import retry
+from time import sleep
 
 src_dir = 'src'
 
@@ -9,14 +12,12 @@ mled = digitalio.DigitalInOut(board.D5)
 mled.direction = digitalio.Direction.OUTPUT
 
 def setup():
-    try:
-        global w
-        global h
-        global display
-        global body
-        global responses
-        global pegelonline_url
+    global w
+    global h
+    global display
+    global body
 
+    try:
         display = epd2in13_V4.EPD()
         display.init()
         display.Clear(255)
@@ -29,6 +30,23 @@ def setup():
     except IOError as e:
         print(e)
         display.sleep()
+
+def start():
+    mled.value = True
+    setup()
+
+    title = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=28)
+    boot = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=18)
+    image = Image.new(mode='1', size=(h, w), color=255)
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 30), 'm~cat v0.4 DEV', font=title, fill=0, align='left')
+    draw.text((0, 55), 'booting ...', font=boot, fill=0, align='left')
+    display.display(display.getbuffer(image))
+    display.sleep()
+
+    global responses
+    global s
+
 
     cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
     retry_session = retry(cache_session, retries=5, backoff_factor= 0.2)
@@ -43,26 +61,12 @@ def setup():
     }
     responses = openmeteo.weather_api(url, params=params)
 
-    pegelonline_url = requests.get("https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/c1f059dd-5063-468a-9dfa-0360fe0b030d/W/currentmeasurement.json")
 
-def start():
-    mled.value = True
-    setup()
-
-    title = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=28)
-    boot = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=18)
-    image = Image.new(mode='1', size=(h, w), color=255)
-    draw = ImageDraw.Draw(image)
-    draw.text((0, 30), 'm~cat v0.4 DEV', font=title, fill=0, align='left')
-    draw.text((0, 55), 'booting ...', font=boot, fill=0, align='left')
-    display.display(display.getbuffer(image))
-    display.sleep()
     time.sleep(15)
 
-def dashboard():
+def overlay():
     display.init()
     display.Clear(255)
-    mled.value = False
 
     image = Image.new(mode='1', size=(h, w), color=255)
     draw = ImageDraw.Draw(image)
@@ -74,28 +78,103 @@ def dashboard():
     wind_direc = current.Variables(2).Value()
     wind_gusts = current.Variables(3).Value()
 
-    temp_ro = round(temp, 2)
+    temp_ro = round(temp, 1)
     windspeed_ro = round(wind_speed, 2)
     windgusts_ro = round(wind_gusts,2)
     winddirec_ro = round(wind_direc, 2)
 
-    wasserstand = pegelonline_url.json()
-
     ltime = time.localtime()
-    fortime = time.strftime("%H:%M", ltime)
+    fortime = time.strftime("%H:%M:%S", ltime)
 
     dash = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=12)
-    time_font = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=28)
-    draw.text((0,0), f"{temp_ro}°C  {windspeed_ro}kn/{windgusts_ro}kn  {winddirec_ro}°  {wasserstand.get('value')}cm", font=dash, fill=0, align='left')
-    draw.text((0,15), f"{fortime}",font=time_font, fill=0, align='left')
+    draw.text((0,0), f"{temp_ro}°C  {windspeed_ro}kn/{windgusts_ro}kn  {winddirec_ro}°", font=dash, fill=0, align='left')
+    draw.text((0, 110), f"last update: {fortime}", font=dash, fill=0, align='left')
     display.display(display.getbuffer(image))
 
     display.sleep()
-    time.sleep(30)
+
+def weather():
+    global activ_site
+    display.init()
+    image = Image.new(mode='1', size=(h, w), color=255)
+    draw = ImageDraw.Draw(image)
+
+    response = responses[0]
+    current = response.Current()
+    temp = current.Variables(0).Value()
+    wind_speed = current.Variables(1).Value()
+    wind_direc = current.Variables(2).Value()
+    wind_gusts = current.Variables(3).Value()
+
+    temp_ro = round(temp, 1)
+    windspeed_ro = round(wind_speed, 2)
+    windgusts_ro = round(wind_gusts, 2)
+    winddirec_ro = round(wind_direc, 2)
+
+    ltime = time.localtime()
+    fortime = time.strftime("%H:%M:%S", ltime)
+
+    text_font = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=14)
+    update_font = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=12)
+    bold = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Bold.ttf'), size=14)
+
+    draw.text((0, 10), f"Zimmertemperatur: 25°C", font=text_font, fill=0, align='left')
+    draw.text((0, 30), f"Daten für Bremen, Habenhausen:", font=bold, fill=0, align='left')
+    draw.text((0, 42), f"Temperatur: {temp_ro}°C", font=text_font, fill=0, align='left')
+    draw.text((0, 54), f"Windgeschwindigkeit: {windspeed_ro}kn", font=text_font, fill=0, align='left')
+    draw.text((0, 66), f"Böengeschwindigkeit: {windgusts_ro}kn", font=text_font, fill=0, align='left')
+    draw.text((0, 78), f"Windrichtung: {winddirec_ro}°", font=text_font, fill=0, align='left')
+
+    draw.text((0, 110), f"last update: {fortime}", font=update_font, fill=0, align='left')
+
+    display.display(display.getbuffer(image))
+    display.sleep()
+    activ_site = 2
+
+def overlay_update():
+    global activ_site
+    display.init()
+    image = Image.new(mode='1', size=(h, w), color=255)
+    draw = ImageDraw.Draw(image)
+
+    response = responses[0]
+    current = response.Current()
+    temp = current.Variables(0).Value()
+    wind_speed = current.Variables(1).Value()
+    wind_direc = current.Variables(2).Value()
+    wind_gusts = current.Variables(3).Value()
+
+    temp_ro = round(temp, 1)
+    windspeed_ro = round(wind_speed, 2)
+    windgusts_ro = round(wind_gusts, 2)
+    winddirec_ro = round(wind_direc, 2)
+
+    ltime = time.localtime()
+    fortime = time.strftime("%H:%M:%S", ltime)
+
+    dash = ImageFont.truetype(font=os.path.join(src_dir, 'Roboto-Regular.ttf'), size=12)
+    draw.text((0, 0), f"{temp_ro}°C  {windspeed_ro}kn/{windgusts_ro}kn  {winddirec_ro}°", font=dash, fill=0, align='left')
+    draw.text((0, 110), f"last update: {fortime}", font=dash, fill=0, align='left')
+    display.displayPartial(display.getbuffer(image))
+
+    display.sleep()
+    activ_site = 1
+
+def update_routine():
+    while True:
+        if activ_site == "1":
+            sleep(300)
+            overlay_update()
+        if activ_site == "2":
+            sleep(1800)
+            weather()
 
 def main():
     start()
-    dashboard()
+    mled.value = False
+    weather()
+    update_routine()
+
 
 if __name__ == "__main__":
     main()
